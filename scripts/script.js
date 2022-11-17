42f8cbd44401f22b5b674619e99ff28a63a83942
gustavoestevesr <gustavo.tech4me@gmail.com>
2022-11-17 19:07:11 -0300

    boardgame new slots questions

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,21 @@ let btnPlay = document.querySelector('#btnPlay')
 let btnCredits = document.querySelector('#btnCredits')
 let btnExit = document.querySelector('#btnExit')
 
+//CHARACTER
+let superDentinho = document.querySelector('#superDentinho')
+
+//SLOT OF QUESTIONS
+let questionSlot1 = document.querySelector('img#q1')
+let questionSlot2 = document.querySelector('img#q2')
+let questionSlot3 = document.querySelector('img#q3')
+let questionSlot4 = document.querySelector('img#q4')
+let questionSlot5 = document.querySelector('img#q5')
+let questionSlot6 = document.querySelector('img#q6')
+let questionSlot7 = document.querySelector('img#q7')
+let questionSlot8 = document.querySelector('img#q8')
+let questionSlot9 = document.querySelector('img#q9')
+let questionSlot10 = document.querySelector('img#q10')
+
 // INITIALIZE
 sectionMenu.style.display = 'flex'
 sectionCredits.style.display = 'none'
@@ -19,15 +34,24 @@ sectionQuestionLeft.style.display = 'none'
 sectionQuestionRight.style.display = 'none'
 sectionAnswer.style.display = 'none'
 
+// DEFINE THE FIRST SCREEN
 document.body.classList.add("background");
+
+// DEFINE THE FIRST POSITION
+superDentinho.classList.add("p1")
+
+// DEFINE THE FIRST ACTIVE SLOT QUESTION
+questionSlot1.classList.add("questionSlotActive")
 
 // NAVIGATE PLAY MENU TO BOARDGAME
 btnPlay.addEventListener("click", function() {
     sectionMenu.style.display = 'none'
     sectionBoardgame.style.display = 'flex'
 
-    document.body.classList.remove("background");
-    document.body.classList.add("boardgame");
+    document.body.classList.remove("background")
+    document.body.classList.add("boardgame")
+
+    playSound()
 });
 
 // NAVIGATE CREDITS MENU TO CREDITS
@@ -37,8 +61,9 @@ btnCredits.addEventListener("click", function() {
 });
 
 // BACKGROUND SOUND
-
-let sound = document.querySelector("#audio");
-sound.currentTime = 0;
-sound.loop = true; //if you want it to restart playing automatically when it ends
-sound.play();
+function playSound(){
+    let sound = document.getElementById("audio")
+    sound.currentTime = 0
+    sound.loop = true
+    sound.play()
+}
