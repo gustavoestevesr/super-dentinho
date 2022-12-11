1c88032414182ca8992783e5f7e5db1759a063a9
gustavoestevesr <gustavo.tech4me@gmail.com>
2022-12-11 16:32:21 -0300

    movement for slot question

--- scripts/script.js
+++ scripts/script.js
@@ -16,10 +16,10 @@ let btnRules = document.querySelector('#btnRules')
 let btnBackFromRulesToApresentation = document.querySelector('#btnBackFromRulesToApresentation')
 let btnBackFromCreditsToApresentation = document.querySelector('#btnBackFromCreditsToApresentation')
 let btnBackFromQuestionToBoardgame = document.querySelector('#btnBackFromQuestionToBoardgame')
-let btnNextFromToBoardgame =  document.querySelector('#btnNextFromToBoardgame')
+let btnNextFromToBoardgame = document.querySelector('#btnNextFromToBoardgame')
 
 //CHARACTER
-let superDentinho = document.querySelector('#superDentinho')
+let superDentinhoBoardGame = document.querySelector('#superDentinhoBoardGame')
 
 //SLOT OF QUESTIONS
 let questionSlot1 = document.querySelector('img#q1')
@@ -60,7 +60,7 @@ sectionRules.style.display = 'none'
 document.body.classList.add("start");
 
 // // DEFINE THE FIRST POSITION
-// superDentinho.classList.add("p1")
+// superDentinhoBoardGame.classList.add("p1")
 
 // DEFINE THE FIRST ACTIVE SLOT QUESTION
 questionSlot1.classList.add("questionSlotActive")
@@ -121,7 +121,7 @@ btnRules.addEventListener("click", function () {
     document.body.classList.remove("background")
     document.body.classList.add("rules")
 
-    displayApresentationRules( true )
+    displayApresentationRules(true)
 });
 
 // NAVIGATE FROM APRESENTATION TO CREDITS
@@ -163,6 +163,9 @@ btnBackFromQuestionToBoardgame.addEventListener("click", function () {
 
     document.body.classList.remove("question")
     document.body.classList.add("boardgame")
+
+    numberQuestion--
+    updateActiveSlotQuestion(numberQuestion)
 });
 
 // EXIT THE GAME
@@ -241,61 +244,157 @@ var questions = [
     },
     {
         title: "E a quantidade de pasta para colocar para idade de vocês?",
-        choices: ["a) a quantidade vai de um lado ao outro da escova (foto mandada da pasta em toda a escova)", "b) a quantidade e relativa a um grão de ervilha"],
-        answer: "b) a quantidade e relativa a um grão de ervilha",
+        choices: ["a quantidade vai de um lado ao outro da escova (foto mandada da pasta em toda a escova)", "a quantidade e relativa a um grão de ervilha"],
+        answer: "a quantidade e relativa a um grão de ervilha",
         correctAnswerExplanation: "Acertou!!! a pasta deve ser colocada na idade de vocês, do mesmo tamanho de um carocinho de ver ilha!! ( foto de uma ervilha)",
         wrongAnswerExplanation: "Errou !!! a pasta deve ser colocada na idade de vocês ,do mesmo tamanho de um carocinho de ver ilha!! ( foto de uma ervilha)"
     },
     {
         title: "Você precisa ir no dentista quando...",
-        choices: ["a) Meu dente doer apenas.", "b) De 6 em 6 meses, mesmo se não estiver sentindo nenhuma dor."],
-        answer: "b) De 6 em 6 meses, mesmo se não estiver sentindo nenhuma dor.",
+        choices: ["Meu dente doer apenas.", "De 6 em 6 meses, mesmo se não estiver sentindo nenhuma dor."],
+        answer: "De 6 em 6 meses, mesmo se não estiver sentindo nenhuma dor.",
         correctAnswerExplanation: "Acertou!!!! você precisa ir no dentista mesmo se seu dentinho não estiver doendo ,para fazer a manutenção da sua saúde da boca.",
         wrongAnswerExplanation: "Errada, você precisa ir no dentista mesmo se seu dentinho não estiver doendo, para fazer a manutenção da sua saúde da bucal."
     },
     {
         title: "A massinha branca que fica sobre os dentes, precisam ser removidas com a escovação?",
-        choices: ["a) sim, caso não seja removida ela poderá causar a cárie ou doença na gengiva", "b) não ,essa massinha é a pasta de dente que ficou no dente.(foto de varias bichinhos no dente, escovando eles somem)"],
-        answer: "a) sim, caso não seja removida ela poderá causar a cárie ou doença na gengiva",
+        choices: ["sim, caso não seja removida ela poderá causar a cárie ou doença na gengiva", "não ,essa massinha é a pasta de dente que ficou no dente.(foto de varias bichinhos no dente, escovando eles somem)"],
+        answer: "sim, caso não seja removida ela poderá causar a cárie ou doença na gengiva",
         correctAnswerExplanation: "correta! Essa massinha e devido a não escovação correta é ela que tem várias bactérias e pode causar a cárie e gengivite (doença que causa inflamação, vermelhão, dor e sangramento na gengiva)",
         wrongAnswerExplanation: "errada! Não é pasta não ! ! Essa massinha e devido a não escovação correta é ela que tem várias bactérias e pode causar a cárie ou gengivite (doença que causa inflamação, vermelhão, dor e sangramento na gengiva)"
     },
     {
         title: "O fio dental é usado antes ou depois da escovação?",
-        choices: ["a) depois da escovação", "b) antes da escovação"],
-        answer: "b) antes da escovação",
+        choices: ["depois da escovação", "antes da escovação"],
+        answer: "antes da escovação",
         correctAnswerExplanation: "Isso aí!! Muito bem sempre antes da escovação passamos o fio dental",
         wrongAnswerExplanation: "errou! ( carinha de triste) sempre antes da escovação passamos o fio dental"
     },
     {
         title: "Alimentos cariogênicos são aqueles que causam cárie. Eles são:",
-        choices: ["a) balas, chocolates, refrigerantes", "b) frutas, legumes e verduras."],
-        answer: "a) balas, chocolates, refrigerantes",
+        choices: ["balas, chocolates, refrigerantes", "frutas, legumes e verduras."],
+        answer: "balas, chocolates, refrigerantes",
         correctAnswerExplanation: "Isso, as balas ,chocolates são ricos em açúcares por isso causam cárie",
         wrongAnswerExplanation: "Esses alimentos não são  carigênicos, ou seja eles não causam a cárie, e ainda contribuem para saúde de uma forma geral prevenindo diabetes ,obesidade e hipertensão"
     },
     {
         title: "Os alimentos autolimpantes são aqueles, que limpam os dentes eles podem ser:",
-        choices: ["a) geleias e chicletes", "b) maças e cenouras"],
-        answer: "b) maças e cenouras",
+        choices: ["geleias e chicletes", "maças e cenouras"],
+        answer: "maças e cenouras",
         correctAnswerExplanation: "sim, eles limpam as faces dos dentes ,contribuindo para uma gengiva saudável",
         wrongAnswerExplanation: "não, pelo contrário esses alimentos agarram nos dentes"
     },
     {
         title: "A língua precisa também ser escovada em todas as escovações do dente?",
-        choices: ["a) uma vez ao dia é o suficiente", "b) sim, todas as escovações precisamos escovar a língua(língua sendo limpa)"],
-        answer: "b) sim, todas as escovações precisamos escovar a língua(língua sendo limpa)",
+        choices: ["uma vez ao dia é o suficiente", "sim, todas as escovações precisamos escovar a língua(língua sendo limpa)"],
+        answer: "sim, todas as escovações precisamos escovar a língua(língua sendo limpa)",
         correctAnswerExplanation: "sim!!!! A língua assim como os dentes ,precisa de cuidados ,todas as vezes que escovamos os dentes ,precisamos escovar a língua também .",
         wrongAnswerExplanation: "errada! A língua assim como os dentes, precisa de cuidados ,todas as vezes que escovamos os dentes ,precisamos escovar a língua também"
     },
     {
-        title: "Undefined does NOT mean that _______.",
-        choices: ["the variable in the code doesn't exist", "the variable is not assigned to a value", "the variable is assigned no value", "the property doesn't exist"],
-        answer: "the variable is assigned no value"
+        title: "Estou com dor de dente na escola, o que devo fazer?",
+        choices: ["Falar com a professora para ela falar com o meu responsável.", "Ficar quieto para não precisar ir ao dentista."],
+        answer: "Falar com a professora para ela falar com o meu responsável.",
+        correctAnswerExplanation: "Correta, sempre que estiver com dor preciso falar a um responsável assim, ele poderá ajudar e leva-lo ao dentista!",
+        wrongAnswerExplanation: "Errada, sempre que estiver com dor preciso falar a um responsável assim ,ele poderá ajudar e leva-lo ao dentista. Não precisa ter  medo pois o dentista irá  para ajudar você a não sentir mais dor! (DENTISTA TRATANDO)"
+    },
+    {
+        title: "O que fazer para não ter cáries?",
+        choices: ["Diminuir os açúcares da comida, e sempre higienizar a boca.", "Manter os açúcares da comida, e higienizar 2 vezes a boca por dia."],
+        answer: "Diminuir os açúcares da comida, e sempre higienizar a boca.",
+        correctAnswerExplanation: "Correto! Diminuindo, e higienizando teremos sempre uma boca livre da doença cárie",
+        wrongAnswerExplanation: "Incorreto! Devemos diminuir da nossa comida os açúcares, como: balas, doces, refrigerantes e higienizar sempre a boca, após todas as alimentações!"
     }
 ];
 
-function changeActiveSlotQuestion( numberQuestion ) {
+function updateDentinhoPosition(numberQuestion) {
+    switch (numberQuestion) {
+        case 0:
+            changeDentinhoPosition(questionSlot1)
+            break;
+
+        case 1:
+            changeDentinhoPosition(questionSlot2)
+            break;
+
+        case 2:
+            changeDentinhoPosition(questionSlot3)
+            break;
+
+        case 3:
+            changeDentinhoPosition(questionSlot4)
+            break;
+
+        case 4:
+            changeDentinhoPosition(questionSlot5)
+            break;
+
+        case 5:
+            changeDentinhoPosition(questionSlot6)
+            break;
+
+        case 6:
+            changeDentinhoPosition(questionSlot7)
+            break;
+
+        case 7:
+            changeDentinhoPosition(questionSlot8)
+            break;
+
+        case 8:
+            changeDentinhoPosition(questionSlot9)
+            break;
+
+        case 9:
+            changeDentinhoPosition(questionSlot10)
+            break;
+
+        case 10:
+            changeDentinhoPosition(questionSlot11)
+            break;
+
+            case 11:
+                changeDentinhoPosition(questionSlot12)
+                break;
+    
+            case 12:
+                changeDentinhoPosition(questionSlot13)
+                break;
+    
+            case 13:
+                changeDentinhoPosition(questionSlot14)
+                break;
+    
+            case 14:
+                changeDentinhoPosition(questionSlot15)
+                break;
+    
+            case 15:
+                changeDentinhoPosition(questionSlot16)
+                break;
+    
+            case 16:
+                changeDentinhoPosition(questionSlot17)
+                break;
+    
+            case 17:
+                changeDentinhoPosition(questionSlot18)
+                break;
+    
+            case 18:
+                changeDentinhoPosition(questionSlot19)
+                break;
+    
+            case 19:
+                changeDentinhoPosition(questionSlot20)
+                break;
+
+        default:
+            break;
+    }
+}
+
+function updateActiveSlotQuestion(numberQuestion) {
     switch (numberQuestion) {
         case 0:
             questionSlot1.classList.remove("questionSlotActive")
@@ -352,39 +451,114 @@ function changeActiveSlotQuestion( numberQuestion ) {
             questionSlot10.classList.add("questionSlotActive")
             break;
 
+        case 11:
+            questionSlot10.classList.remove("questionSlotActive")
+            questionSlot11.classList.add("questionSlotActive")
+            break;
+
+        case 12:
+            questionSlot11.classList.remove("questionSlotActive")
+            questionSlot12.classList.add("questionSlotActive")
+            break;
+
+        case 13:
+            questionSlot12.classList.remove("questionSlotActive")
+            questionSlot13.classList.add("questionSlotActive")
+            break;
+
+        case 14:
+            questionSlot13.classList.remove("questionSlotActive")
+            questionSlot14.classList.add("questionSlotActive")
+            break;
+
+        case 15:
+            questionSlot14.classList.remove("questionSlotActive")
+            questionSlot15.classList.add("questionSlotActive")
+            break;
+
+        case 16:
+            questionSlot15.classList.remove("questionSlotActive")
+            questionSlot16.classList.add("questionSlotActive")
+            break;
+
+        case 17:
+            questionSlot16.classList.remove("questionSlotActive")
+            questionSlot17.classList.add("questionSlotActive")
+            break;
+
+        case 18:
+            questionSlot17.classList.remove("questionSlotActive")
+            questionSlot18.classList.add("questionSlotActive")
+            break;
+
+        case 19:
+            questionSlot18.classList.remove("questionSlotActive")
+            questionSlot19.classList.add("questionSlotActive")
+            break;
+
+        case 20:
+            questionSlot19.classList.remove("questionSlotActive")
+            questionSlot20.classList.add("questionSlotActive")
+            break;
+
         default:
             break;
     }
 }
 
 function prepareQuestion() {
-    document.querySelector("p#numberQuestion.apresentationText").innerHTML = `${numberQuestion+1}° Questão`
+    document.querySelector("p#numberQuestion.apresentationText").innerHTML = `${numberQuestion + 1}° Questão`
     document.querySelector("p#question.apresentationText").innerHTML = `${questions[numberQuestion].title}`
     document.querySelector("p#option1.apresentationText").innerHTML = `${questions[numberQuestion].choices[0]}`
     document.querySelector("p#option2.apresentationText").innerHTML = `${questions[numberQuestion].choices[1]}`
 }
 
-function goToQuestion( slotSelectedFromUSer ){
-     if (slotSelectedFromUSer === numberQuestion) {
+function delay(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+function changeDentinhoPosition(slotNumber) {
+    // change position
+    document.addEventListener('click', function handleClick(event) {
+        superDentinhoBoardGame.style.top = getOffset(slotNumber).top + 'px';
+        superDentinhoBoardGame.style.left = getOffset(slotNumber).left + 'px';
+    });
+}
+
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
+
+async function goToQuestion(slotSelectedFromUSer) {
+
+    if (slotSelectedFromUSer === numberQuestion) {
+        updateDentinhoPosition(numberQuestion)
+        await delay(500);
+        updateActiveSlotQuestion(numberQuestion)
+
         // change background
         sectionBoardgame.style.display = 'none'
         sectionQuestion.style.display = 'flex'
-    
+
         document.body.classList.remove("background")
         document.body.classList.add("question")
-    
+
         prepareQuestion()
-     }
+    }
 }
 
-function verifyQuestion( userChoice ) {
+function verifyQuestion(userChoice) {
     let answerUser = userChoice === "option1" ? questions[numberQuestion].choices[0] : questions[numberQuestion].choices[1]
 
     let statusAnswerUser = questions[numberQuestion].answer === answerUser ? true : false
 
-    changeActiveSlotQuestion(numberQuestion)
     feedbackAnswer(statusAnswerUser)
-    numberQuestion++;
 }
 
 function prepareAnswerExplanation() {
@@ -393,7 +567,7 @@ function prepareAnswerExplanation() {
     document.querySelector("p#correctAnswerExplanation.apresentationText").innerHTML = `${questions[numberQuestion].correctAnswerExplanation}`
 }
 
-function feedbackAnswer( statusAnswerUser ) {
+function feedbackAnswer(statusAnswerUser) {
     // change background
     sectionAnswer.style.display = 'flex'
     sectionQuestion.style.display = 'none'
@@ -402,12 +576,16 @@ function feedbackAnswer( statusAnswerUser ) {
     document.body.classList.add("answer")
 
     if (statusAnswerUser) {
+        document.querySelector("img#superDentinhoAnswer").src = "images/characters/2712.png"
         playCorrectSound()
         start();
         stop();
     } else {
+        document.querySelector("img#superDentinhoAnswer").src = "images/characters/2702.png"
         playWrongSound()
     }
 
+    numberQuestion++;
     prepareAnswerExplanation()
+
 }
